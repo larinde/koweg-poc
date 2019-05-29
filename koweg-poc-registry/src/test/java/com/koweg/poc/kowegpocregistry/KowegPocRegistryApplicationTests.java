f1ee9c1ba61855b1f12c30ccb4a05a3567a06d4c
larinde <olarinde.ajai@gmail.com>
2019-05-29 02:15:11 +0100

    mixed

--- koweg-poc-registry/src/test/java/com/koweg/poc/kowegpocregistry/KowegPocRegistryApplicationTests.java
+++ koweg-poc-registry/src/test/java/com/koweg/poc/kowegpocregistry/KowegPocRegistryApplicationTests.java
@@ -9,8 +9,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class KowegPocRegistryApplicationTests {
 
+/*
 	@Test
 	public void contextLoads() {
 	}
-
+*/
 }
